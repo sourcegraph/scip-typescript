b54495ca29fd1ca97e825d93871e6f6506ed0461
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-04-04 15:58:35 -0700

    De-alias symbols similar to tsserver's go-to-definition.
    Fixes issue #46.

--- snapshots/output/react/src/UseMyTSXElement.tsx
+++ snapshots/output/react/src/UseMyTSXElement.tsx
@@ -2,14 +2,14 @@
 //       ^^^^^ reference @types/react 17.0.0 `index.d.ts`/React/
   
   import { MyProps, MyTSXElement } from "./MyTSXElement";
-//         ^^^^^^^ reference local 0
-//                  ^^^^^^^^^^^^ reference @types/react 17.0.0 `index.d.ts`/React/FunctionComponent#
+//         ^^^^^^^ reference react-example 1.0.0 src/`MyTSXElement.tsx`/MyProps#
+//                  ^^^^^^^^^^^^ reference react-example 1.0.0 src/`MyTSXElement.tsx`/MyTSXElement.
   
   export const _: React.FunctionComponent<MyProps> =
 //             ^ definition react-example 1.0.0 src/`UseMyTSXElement.tsx`/_.
 //                ^^^^^ reference @types/react 17.0.0 `index.d.ts`/React/
 //                      ^^^^^^^^^^^^^^^^^ reference @types/react 17.0.0 `index.d.ts`/React/FunctionComponent#
-//                                        ^^^^^^^ reference local 0
+//                                        ^^^^^^^ reference react-example 1.0.0 src/`MyTSXElement.tsx`/MyProps#
       ({}) => (<MyTSXElement></MyTSXElement>)
-//              ^^^^^^^^^^^^ reference @types/react 17.0.0 `index.d.ts`/React/FunctionComponent#
-//                             ^^^^^^^^^^^^ reference @types/react 17.0.0 `index.d.ts`/React/FunctionComponent#
+//              ^^^^^^^^^^^^ reference react-example 1.0.0 src/`MyTSXElement.tsx`/MyTSXElement.
+//                             ^^^^^^^^^^^^ reference react-example 1.0.0 src/`MyTSXElement.tsx`/MyTSXElement.
---
+++ src/TypeScriptInternal.ts
@@ -0,0 +1,38 @@
+import * as ts from 'typescript'
+
+// Functions in this file are based directly off corresponding functions
+// in the TypeScript codebase.
+
+export function shouldSkipAlias(node: ts.Node): boolean {
+  switch (node.kind) {
+    case ts.SyntaxKind.ImportClause:
+    case ts.SyntaxKind.ImportEqualsDeclaration:
+      // TODO: How do we test this code path?
+      return true;
+    case ts.SyntaxKind.ImportSpecifier:
+      return node.parent.kind === ts.SyntaxKind.NamedImports;
+    case ts.SyntaxKind.BindingElement:
+    case ts.SyntaxKind.VariableDeclaration:
+      // TODO: How do we test this code path?
+      return isInJSFile(node) && isVariableDeclarationInitializedToBareOrAccessedRequire(node);
+    default:
+      return false;
+  }
+}
+
+function isInJSFile(node: ts.Node): boolean {
+  return !!(node.flags & ts.NodeFlags.JavaScriptFile)
+}
+
+function isVariableDeclarationInitializedToBareOrAccessedRequire(node: ts.Node): boolean {
+  if (node.kind === ts.SyntaxKind.BindingElement) {
+    node = node.parent.parent;
+  }
+  return isVariableDeclaration(node) && !!node.initializer;
+  // FIXME: This requires inlining a bunch of more definitions.
+  //       ts.isRequireCall(allowAccessedRequire ? ts.getLeftmostAccessExpression(node.initializer) : node.initializer, /*requireStringLiteralLikeArgument*/ true);
+}
+
+function isVariableDeclaration(node: ts.Node): node is ts.VariableDeclaration {
+  return node.kind === ts.SyntaxKind.VariableDeclaration;
+}
