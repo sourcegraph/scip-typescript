b7886998a295d8a61c8d2a5431debe4233b876c6
Ólafur Páll Geirsson <olafurpg@gmail.com>
2022-07-28 10:52:46 +0200

    Print message when npm/yarn install fails during auto-indexing
    Previously, auto-indexing jobs silently ignored npm/yarn install
    failures making it difficult to understand why the failures were
    ignored.

--- dev/lenient-yarn.sh
+++ dev/lenient-yarn.sh
@@ -2,7 +2,7 @@
 set -eux
 
 if [ $# -gt 1 ] && [ "$1" == "install" ]; then
-	/usr/local/bin/actual-yarn "$@" || true
+	/usr/local/bin/actual-yarn "$@" || echo "scip-typescript: ignoring yarn install failure, will try to auto-index the project with partial dependency information"
 else
 	/usr/local/bin/actual-yarn "$@"
 fi
