e62facf6dfc36ac141c40162a206fb416ca2483e
Valery Bugakov <skymk1@gmail.com>
2022-10-28 12:44:31 +0800

    Fix the `import` test

--- snapshots/output/syntax/src/import.ts
+++ snapshots/output/syntax/src/import.ts
@@ -34,7 +34,7 @@
       newFunction() +
 //    ^^^^^^^^^^^ reference syntax 1.0.0 src/`function.ts`/newFunction().
       namespace.a.value
-//    ^^^^^^^^^ reference local 0
+//    ^^^^^^^^^ reference syntax 1.0.0 src/`namespace.ts`/
 //              ^ reference syntax 1.0.0 src/`namespace.ts`/a/
 //                ^^^^^ reference syntax 1.0.0 src/`namespace.ts`/a/value.
     )
@@ -51,9 +51,9 @@
     return import('./function').then(c => c.newFunction())
 //                ^^^^^^^^^^^^ reference syntax 1.0.0 src/`function.ts`/
 //                              ^^^^ reference typescript 4.8.4 lib/`lib.es5.d.ts`/Promise#then().
-//                                   ^ definition local 4
+//                                   ^ definition local 3
 //                                   documentation ```ts\n(parameter) c: typeof import("/src/function")\n```
-//                                        ^ reference local 4
+//                                        ^ reference local 3
 //                                          ^^^^^^^^^^^ reference syntax 1.0.0 src/`function.ts`/newFunction().
   }
   
