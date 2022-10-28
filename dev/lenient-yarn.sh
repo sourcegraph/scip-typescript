423b8f6e6b4280a6d16e67a849422d9f4c11012e
Noah S-C <noah@sourcegraph.com>
2022-10-28 15:03:40 +0100

    codeintel: add ignore-engines based on yarn version (#197)

--- dev/lenient-yarn.sh
+++ dev/lenient-yarn.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -eux
 
+ADDITIONAL_ARGS="--ignore-engines"
+if [ "$(/usr/local/bin/actual-yarn --version | cut -d'.' -f1)" -gt 1 ]; then
+	ADDITIONAL_ARGS=""
+fi
+
 if [ $# -gt 1 ] && [ "$1" == "install" ]; then
-	/usr/local/bin/actual-yarn "$@" || echo "scip-typescript: ignoring yarn install failure, will try to auto-index the project with partial dependency information"
+	/usr/local/bin/actual-yarn $ADDITIONAL_ARGS "$@" || echo "scip-typescript: ignoring yarn install failure, will try to auto-index the project with partial dependency information"
 else
-	/usr/local/bin/actual-yarn "$@"
+	/usr/local/bin/actual-yarn $ADDITIONAL_ARGS "$@"
 fi
