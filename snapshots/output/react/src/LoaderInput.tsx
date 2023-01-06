d0bc3647765527d39a23bfa43526de654d510762
Valery Bugakov <skymk1@gmail.com>
2023-01-06 14:54:00 +0800

    add `pnpm` to `devDependencies`

--- snapshots/output/react/src/LoaderInput.tsx
+++ snapshots/output/react/src/LoaderInput.tsx
@@ -1,8 +1,8 @@
   import React from 'react'
 // definition react-example 1.0.0 src/`LoaderInput.tsx`/
 //documentation ```ts\nmodule "LoaderInput.tsx"\n```
-//       ^^^^^ reference @types/react 17.0.0 `index.d.ts`/React/
-//                  ^^^^^^^ reference @types/react 17.0.0 `index.d.ts`/
+//       ^^^^^ reference @types/react 17.0.52 `index.d.ts`/React/
+//                  ^^^^^^^ reference @types/react 17.0.52 `index.d.ts`/
   
   /** Takes loading prop, input component as child */
   interface Props {
@@ -15,15 +15,15 @@
     children: React.ReactNode
 //  ^^^^^^^^ definition react-example 1.0.0 src/`LoaderInput.tsx`/Props#children.
 //  documentation ```ts\n(property) children: ReactNode\n```
-//            ^^^^^ reference @types/react 17.0.0 `index.d.ts`/React/
-//                  ^^^^^^^^^ reference @types/react 17.0.0 `index.d.ts`/React/ReactNode#
+//            ^^^^^ reference @types/react 17.0.52 `index.d.ts`/React/
+//                  ^^^^^^^^^ reference @types/react 17.0.52 `index.d.ts`/React/ReactNode#
   }
   
   export const LoaderInput: React.FunctionComponent<Props> = ({
 //             ^^^^^^^^^^^ definition react-example 1.0.0 src/`LoaderInput.tsx`/LoaderInput.
 //             documentation ```ts\nvar LoaderInput: FunctionComponent<Props>\n```
-//                          ^^^^^ reference @types/react 17.0.0 `index.d.ts`/React/
-//                                ^^^^^^^^^^^^^^^^^ reference @types/react 17.0.0 `index.d.ts`/React/FunctionComponent#
+//                          ^^^^^ reference @types/react 17.0.52 `index.d.ts`/React/
+//                                ^^^^^^^^^^^^^^^^^ reference @types/react 17.0.52 `index.d.ts`/React/FunctionComponent#
 //                                                  ^^^^^ reference react-example 1.0.0 src/`LoaderInput.tsx`/Props#
     loading,
 //  ^^^^^^^ definition local 3
@@ -36,23 +36,23 @@
 //  ^^^^^^^^ reference local 7
   }) => (
     <div className="hello">
-//   ^^^ reference @types/react 17.0.0 `index.d.ts`/global/JSX/IntrinsicElements#div.
-//       ^^^^^^^^^ reference @types/react 17.0.0 `index.d.ts`/React/HTMLAttributes#className.
+//   ^^^ reference @types/react 17.0.52 `index.d.ts`/global/JSX/IntrinsicElements#div.
+//       ^^^^^^^^^ reference @types/react 17.0.52 `index.d.ts`/React/HTMLAttributes#className.
       {children}
 //     ^^^^^^^^ reference local 4
       {loading && <p>spinner</p>}
 //     ^^^^^^^ reference local 3
-//                 ^ reference @types/react 17.0.0 `index.d.ts`/global/JSX/IntrinsicElements#p.
-//                            ^ reference @types/react 17.0.0 `index.d.ts`/global/JSX/IntrinsicElements#p.
+//                 ^ reference @types/react 17.0.52 `index.d.ts`/global/JSX/IntrinsicElements#p.
+//                            ^ reference @types/react 17.0.52 `index.d.ts`/global/JSX/IntrinsicElements#p.
     </div>
-//    ^^^ reference @types/react 17.0.0 `index.d.ts`/global/JSX/IntrinsicElements#div.
+//    ^^^ reference @types/react 17.0.52 `index.d.ts`/global/JSX/IntrinsicElements#div.
   )
   
   export const LoaderInput2: React.FunctionComponent<Props> = props => {
 //             ^^^^^^^^^^^^ definition react-example 1.0.0 src/`LoaderInput.tsx`/LoaderInput2.
 //             documentation ```ts\nvar LoaderInput2: FunctionComponent<Props>\n```
-//                           ^^^^^ reference @types/react 17.0.0 `index.d.ts`/React/
-//                                 ^^^^^^^^^^^^^^^^^ reference @types/react 17.0.0 `index.d.ts`/React/FunctionComponent#
+//                           ^^^^^ reference @types/react 17.0.52 `index.d.ts`/React/
+//                                 ^^^^^^^^^^^^^^^^^ reference @types/react 17.0.52 `index.d.ts`/React/FunctionComponent#
 //                                                   ^^^^^ reference react-example 1.0.0 src/`LoaderInput.tsx`/Props#
 //                                                            ^^^^^ definition local 9
 //                                                            documentation ```ts\n(parameter) props: PropsWithChildren<Props>\n```
