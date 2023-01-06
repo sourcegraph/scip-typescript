56506add69930f153d7f51787963fc0902aef330
Valery Bugakov <skymk1@gmail.com>
2023-01-06 14:49:40 +0800

    add `pnpm` integration tests

---
+++ snapshots/output/pnpm-workspaces/packages/a/src/a.ts
@@ -0,0 +1,8 @@
+  export function a(): string {
+// definition @example/a 1.0.0 src/`a.ts`/
+//documentation ```ts\nmodule "a.ts"\n```
+//                ^ definition @example/a 1.0.0 src/`a.ts`/a().
+//                documentation ```ts\nfunction a(): string\n```
+    return ''
+  }
+  
