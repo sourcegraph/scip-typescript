ea9aa360be0befc99616ecb80b3dfbb1e7b331d3
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-12-22 11:37:17 +0530

    chore: Run prettier again because I don't have anything else to do with my life on a Friday morning

--- src/FileIndexer.ts
+++ src/FileIndexer.ts
@@ -303,8 +303,7 @@ export class FileIndexer {
 
   private pushOccurrence(occurrence: scip.scip.Occurrence): void {
     if (this.document.occurrences.length > 0) {
-      const lastOccurrence =
-        this.document.occurrences.at(-1)
+      const lastOccurrence = this.document.occurrences.at(-1)
       if (isEqualOccurrence(lastOccurrence, occurrence)) {
         return
       }
