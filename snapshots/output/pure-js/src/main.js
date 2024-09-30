35fcb9afb0d83ee6f808c4a3a3586ccff10d0c64
Christoph <christoph.hegemann@sourcegraph.com>
2024-09-30 10:04:05 +0200

    fix tests

--- snapshots/output/pure-js/src/main.js
+++ snapshots/output/pure-js/src/main.js
@@ -99,8 +99,9 @@ function var_function_scope() {
 
 function array_of_objects() {
 //       ^^^^^^^^^^^^^^^^ definition pure-js 1.0.0 src/`main.js`/array_of_objects().
-  [{ element: 0 }, { element: 1}]
-//   ^^^^^^^ definition pure-js 1.0.0 src/`main.js`/element0:
-//                   ^^^^^^^ definition pure-js 1.0.0 src/`main.js`/element1:
+  var a = [{ element: 0 }, { element: 1 }]
+//    ^ definition local 20
+//           ^^^^^^^ definition pure-js 1.0.0 src/`main.js`/element0:
+//                           ^^^^^^^ definition pure-js 1.0.0 src/`main.js`/element1:
 }
 
