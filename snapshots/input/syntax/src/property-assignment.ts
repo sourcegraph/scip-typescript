950d671f04808441ca5a9dc3a3d9cd6cb8d7f516
Christoph <christoph.hegemann@sourcegraph.com>
2024-09-30 10:14:22 +0200

    adds a test showing the approach fails for quoted object properties

--- snapshots/input/syntax/src/property-assignment.ts
+++ snapshots/input/syntax/src/property-assignment.ts
@@ -5,7 +5,7 @@ export function shorthandPropertyAssignment() {
   const a = 'a'
   return { a }
 }
-type A = { a: string }
+type A = { a: string, b : number }
 export function typedPropertyAssignment(): A {
-  return { a: 'a' }
+  return { a: 'a', "b": 10 }
 }
--- snapshots/output/syntax/src/property-assignment.ts
+++ snapshots/output/syntax/src/property-assignment.ts
@@ -13,13 +13,15 @@ export function shorthandPropertyAssignment() {
 //         ^ definition syntax 1.0.0 src/`property-assignment.ts`/a1:
 //         ^ reference local 2
 }
-type A = { a: string }
+type A = { a: string, b : number }
 //   ^ definition syntax 1.0.0 src/`property-assignment.ts`/A#
 //         ^ definition syntax 1.0.0 src/`property-assignment.ts`/A#typeLiteral3:a.
+//                    ^ definition syntax 1.0.0 src/`property-assignment.ts`/A#typeLiteral3:b.
 export function typedPropertyAssignment(): A {
 //              ^^^^^^^^^^^^^^^^^^^^^^^ definition syntax 1.0.0 src/`property-assignment.ts`/typedPropertyAssignment().
 //                                         ^ reference syntax 1.0.0 src/`property-assignment.ts`/A#
-  return { a: 'a' }
+  return { a: 'a', "b": 10 }
 //         ^ reference syntax 1.0.0 src/`property-assignment.ts`/A#typeLiteral3:a.
+//                 ^^^ definition syntax 1.0.0 src/`property-assignment.ts`/`"b"0`:
 }
 
